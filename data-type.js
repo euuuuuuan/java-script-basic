e1fa52796e93395f545dc5ac0dde35edc5760b21
euan.may24@gmail.com <euan.may24@gmail.com>
2024-06-20 00:11:11 +0900

    java basic

---
+++ data-type.js
@@ -0,0 +1,20 @@
+/* Integer (정수) */
+
+/* Float (소수) */
+
+/* String (문자열)
+* 'javascript' or "javascript"
+* 처음 시작 문자열과 끝의 문자열의 따옴표가 같아야 한다.
+*
+* "Hello" + "World"
+* -> "HelloWorld" 문자열의 연결에도 + 연산이 사용된다.
+* */
+
+/* Boolean (불린) - 참 거짓
+* true or false
+* 8 > 6 true
+* 8 < 6 false
+* */
+console.log(8 > 6);
+console.log(8 < 6);
+
--- first-console.js
+++ first-console.js
@@ -2,3 +2,10 @@ console.log(15)
 console.log(20-5)
 console.log(25-10)
 console.log(3*5)
+
+// 자바스크립트는 한줄에 하나씩만 출력을 하기 때문에
+// 위 코드는 문제가 없다. (세미콜론 유무)
+
+console.log(10); console.log(20 - 5)
+// 세미콜론이 사이에 없을 시 Syntax error 발생한다.
+// 세미콜론을 의식적으로 작성하여 문장의 끝을 구분짓자.
