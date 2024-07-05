3b56b53be24e80147425099c525b0ee7a3e611c9
euan.may24@gmail.com <euan.may24@gmail.com>
2024-07-05 22:30:27 +0900

    함수 선언과 호출

---
+++ function.js
@@ -0,0 +1,15 @@
+/* 변수 = 값
+*  함수 = 명령들
+* */
+
+// 함수 선언
+function greeting() {
+    console.log('Hi');
+    console.log('안녕');
+    console.log('곤니찌와');
+    console.log('짜이찌엔');
+    console.log('본줘르');
+}
+
+// 함수 호출
+greeting();
