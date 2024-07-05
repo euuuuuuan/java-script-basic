b45929911ad165661c301a57467986e836f1a3db
euan.may24@gmail.com <euan.may24@gmail.com>
2024-07-05 23:42:42 +0900

    형변환 산술연산

--- type-conversion.js
+++ type-conversion.js
@@ -41,3 +41,25 @@ console.log(typeof Number(y));
 
 // ''같은 빈문자열이나 숫자 0도, NaN도 false로 변환된다.
 // 이러한 값들을 falsy값이라고 한다.
+
+console.log('')
+// 산술 연산 (+, -, *, /, %, **)
+console.log(4 + '2');  // 42
+console.log(4 + 2);  // 6
+console.log(4 - true);  // 3
+console.log(4 * false);  // 0
+console.log(4 / '2');  // 2
+console.log('4' ** true);  // 4
+console.log(4 % 'two');  // NaN
+
+// 관계 비교 연산 ( <, <=, >, >=)
+console.log(2 < '3');  // true
+console.log(2 > true);  // true
+console.log('2' <= false);  // false
+console.log('two' >= 1);  // false
+
+// 같음 비교 연산 (===, !==, ==, !=)
+console.log(1 === '1');  // 일치, 불일치(!==) // false
+console.log(1 === true);  // false
+console.log(1 == '1');  // 동등, 부등(!=)  // true
+console.log(1 == true);  // true
